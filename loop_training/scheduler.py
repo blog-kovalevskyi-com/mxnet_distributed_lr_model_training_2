e69d0293ff9b7be77e3c4fa10d296283f6433d12
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2017-08-20 19:05:50 -0700

    loop training added.

---
+++ loop_training/scheduler.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ.update({
+  "DMLC_ROLE": "scheduler",
+  "DMLC_PS_ROOT_URI": "127.0.0.1",
+  "DMLC_PS_ROOT_PORT": "9000",
+  "DMLC_NUM_SERVER": "1",
+  "DMLC_NUM_WORKER": "2",
+  "PS_VERBOSE": "0"
+})
+import mxnet
